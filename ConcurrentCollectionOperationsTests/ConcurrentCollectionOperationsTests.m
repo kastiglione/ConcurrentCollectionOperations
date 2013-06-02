238081f5a2c684bc026f93806e609e1f56d17995
Dave Lee <davelee.com@gmail.com>
2013-06-02 14:18:09 -0600

    Delete tests template code

--- ConcurrentCollectionOperationsTests/ConcurrentCollectionOperationsTests.m
+++ ConcurrentCollectionOperationsTests/ConcurrentCollectionOperationsTests.m
@@ -10,23 +10,4 @@
 
 @implementation ConcurrentCollectionOperationsTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in ConcurrentCollectionOperationsTests");
-}
-
 @end
