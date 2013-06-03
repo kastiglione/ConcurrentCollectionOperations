49058922509d1a7263771b37d3aee5a0ad54c5a6
Dave Lee <davelee.com@gmail.com>
2013-06-02 21:04:43 -0600

    Clean-up tests; mostly removing unnecessary tests

--- ConcurrentCollectionOperationsTests/ConcurrentCollectionOperationsTests.m
+++ ConcurrentCollectionOperationsTests/ConcurrentCollectionOperationsTests.m
@@ -24,60 +24,23 @@
 
 #pragma mark - NSArray
 
-- (void)testIdentityMap {
-    NSArray *mapped = [self.numbers cco_concurrentMap:^(NSNumber *number) {
-        return number;
-    }];
-    STAssertEqualObjects(mapped, self.numbers, @"Failed to perform identy map");
-}
-
-- (void)testDelaysMap {
-    NSArray *mapped = [self.numbers cco_concurrentMap:^(NSNumber *number) {
-        sleep(arc4random() % 2);
-        return number;
-    }];
-    STAssertEqualObjects(mapped, self.numbers, @"Failed to perform map with delays");
-}
-
-- (void)testDoublingMap {
+- (void)testArrayDoublingMap {
     NSArray *mapped = [self.numbers cco_concurrentMap:^(NSNumber *number) {
         return @(2 * number.unsignedIntegerValue);
     }];
     NSArray *doubled = @[ @0, @2, @4, @6, @8, @10, @12, @14, @16, @18 ];
-    STAssertEqualObjects(mapped, doubled, @"Failed to perform doubling map");
+    STAssertEqualObjects(mapped, doubled, @"Failed to perform array doubling map");
 }
 
-- (void)testIdentityFilter {
-    NSArray *filtered = [self.numbers cco_concurrentFilter:^BOOL (NSNumber *number) {
-        return YES;
-    }];
-    STAssertEqualObjects(filtered, self.numbers, @"Failed to perform identy filter");
-}
-
-- (void)testDelaysFilter {
-    NSArray *filtered = [self.numbers cco_concurrentFilter:^(NSNumber *number) {
-        sleep(arc4random() % 2);
-        return YES;
-    }];
-    STAssertEqualObjects(filtered, self.numbers, @"Failed to perform filter with delays");
-}
-
-- (void)testEvenFilter {
+- (void)testArrayEvenFilter {
     NSArray *filtered = [self.numbers cco_concurrentFilter:^BOOL (NSNumber *number) {
         return number.unsignedIntegerValue % 2 == 0;
     }];
     NSArray *evens = @[ @0, @2, @4, @6, @8 ];
-    STAssertEqualObjects(filtered, evens, @"Failed for filter for evens");
+    STAssertEqualObjects(filtered, evens, @"Failed for filter array for evens");
 }
 
 #pragma mark - NSDictionary
-
-- (void)testDictionaryIdentityMap {
-    NSDictionary *mapped = [self.letters cco_concurrentMap:^(NSString *letter) {
-        return letter;
-    }];
-    STAssertEqualObjects(mapped, self.letters, @"Failed to perform dictionary identy map");
-}
 
 - (void)testDictionaryAppendingMap {
     NSDictionary *mapped = [self.letters cco_concurrentMap:^(NSString *letter) {
@@ -85,13 +48,6 @@
     }];
     NSDictionary *appended = @{ @0: @"A!", @1: @"B!", @2: @"C!", @3: @"D!", @4: @"E!" };
     STAssertEqualObjects(mapped, appended, @"Failed to perform dictionary appending map");
-}
-
-- (void)testDictionaryIdentityFilter {
-    NSDictionary *filtered = [self.letters cco_concurrentFilter:^BOOL (NSString *letter) {
-        return YES;
-    }];
-    STAssertEqualObjects(filtered, self.letters, @"Failed to perform dictionary identy filter");
 }
 
 - (void)testDictionaryVowelFilter {
