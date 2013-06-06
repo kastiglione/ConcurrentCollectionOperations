6012d2667fe5cb6c73273fd9f5c7ebe53f519c8d
CodaFi <devteam.codafi@gmail.com>
2013-06-05 22:29:49 -0600

    Show NSMapTable some love

---
+++ ConcurrentCollectionOperations/NSMapTable+ConcurrentCollectionOperations.m
@@ -0,0 +1,61 @@
+//
+//  NSMapTable+ConcurrentCollectionOperations.m
+//  ConcurrentCollectionOperations
+//
+//  Created by Robert Widmann on 6/5/13.
+//  Copyright (c) 2013 Internet. All rights reserved.
+//
+
+#import "NSMapTable+ConcurrentCollectionOperations.h"
+
+@implementation NSMapTable (ConcurrentCollectionOperations)
+
+- (instancetype)cco_concurrentMap:(CCOMapBlock)mapBlock {
+    NSParameterAssert(mapBlock != nil);
+	
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    return [self cco_concurrentWithQueue:queue map:mapBlock];
+}
+
+- (instancetype)cco_concurrentWithQueue:(dispatch_queue_t)queue map:(CCOMapBlock)mapBlock {
+    NSParameterAssert(mapBlock != nil);
+	
+	NSMapTable *result = NSCopyMapTableWithZone(self, NULL);
+	NSResetMapTable(result);
+	
+    NSArray *keys = NSAllMapTableKeys(self);
+    NSArray *objects = NSAllMapTableValues(self);
+	
+    dispatch_apply(self.count, queue, ^(size_t i) {
+        [result setObject:mapBlock(objects[i]) forKey:keys[i]];
+    });
+	
+    return result;
+}
+
+- (instancetype)cco_concurrentFilter:(CCOPredicateBlock)predicateBlock {
+    NSParameterAssert(predicateBlock != nil);
+	
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    return [self cco_concurrentWithQueue:queue filter:predicateBlock];
+}
+
+- (instancetype)cco_concurrentWithQueue:(dispatch_queue_t)queue filter:(CCOPredicateBlock)predicateBlock {
+    NSParameterAssert(predicateBlock != nil);
+	
+	NSMapTable *result = NSCopyMapTableWithZone(self, NULL);
+	NSResetMapTable(result);
+	
+    NSArray *keys = NSAllMapTableKeys(self);
+    NSMutableArray *objects = NSAllMapTableValues(self).mutableCopy;
+	
+    dispatch_apply(self.count, queue, ^(size_t i) {
+        if (predicateBlock(objects[i])) {
+            [result setObject:objects[i] forKey:keys[i]];
+        }
+    });
+    
+    return result;
+}
+
+@end
